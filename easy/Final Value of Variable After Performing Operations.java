023f0eb7001f38b74312ddfc0ba6b44d88ee93a8
svkawar <81874751+svkawar@users.noreply.github.com>
2024-07-28 14:31:06 +0530

    Update Final Value of Variable After Performing Operations.java

--- easy/Final Value of Variable After Performing Operations.java
+++ easy/Final Value of Variable After Performing Operations.java
@@ -1,1 +1,14 @@
-Final Value of Variable After Performing Operations
+// Final Value of Variable After Performing Operations
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for(String s : operations) {
+            if(s.equals("++X") || s.equals("X++")) {
+                x++;
+            } else if(s.equals("--X") || s.equals("X--")) {
+                x--;
+            }
+        }
+        return x;        
+    }
+}
