f9d36366ab4bd7b94d7966d7e46a83f4866da4fe
svkawar <81874751+svkawar@users.noreply.github.com>
2024-07-28 14:46:59 +0530

    Create PermutationDiff.java

---
+++ easy/PermutationDiff.java
@@ -0,0 +1,10 @@
+// Permutation Difference between Two Strings
+class Solution {
+    public int findPermutationDifference(String s, String t) {
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++) {
+            sum += Math.abs( i - t.indexOf(s.charAt(i)));
+        }
+        return sum;        
+    }
+}
